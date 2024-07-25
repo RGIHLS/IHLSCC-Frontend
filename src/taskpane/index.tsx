fffbee19de00b8d59acf8047454f5f9f48252f3d
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-07-25 11:45:44 +0100

    chore: Remove react-hot-loader from taskpane index

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -1,13 +1,12 @@
 /**
  * This file initializes the add-in and renders the App component after Office initializes.
- * It imports the necessary components and libraries, including App, AppContainer, initializeIcons, ThemeProvider, React, and ReactDOM.
+ * It imports the necessary components and libraries, including App, initializeIcons, ThemeProvider, React, and ReactDOM.
  * It also defines the title of the add-in and sets the isOfficeInitialized flag to false initially.
  * The render function is called with the App component and the title and isOfficeInitialized props passed in.
  * The Office.onReady function sets the isOfficeInitialized flag to true and calls the render function with the App component.
  * If the module is hot, the App component is re-rendered when changes are made.
  */
 import App from "./components/App";
-import { AppContainer } from "react-hot-loader";
 import { initializeIcons } from "@fluentui/font-icons-mdl2";
 import { ThemeProvider } from "@fluentui/react";
 import * as React from "react";
@@ -23,23 +22,20 @@ const title = "IHLS CC Add-in";
 
 const render = (Component) => {
   ReactDOM.render(
-    <AppContainer>
-      <ThemeProvider>
-        <Component title={title} isOfficeInitialized={isOfficeInitialized} />
-      </ThemeProvider>
-    </AppContainer>,
+    <ThemeProvider>
+      <Component title={title} isOfficeInitialized={isOfficeInitialized} />
+    </ThemeProvider>,
     document.getElementById("container")
   );
 };
 
-/* Render application after Office initializes */
 Office.onReady(() => {
   isOfficeInitialized = true;
   render(App);
 });
 
-if ((module as any).hot) {
-  (module as any).hot.accept("./components/App", () => {
+if (module.hot) {
+  module.hot.accept("./components/App", () => {
     const NextApp = require("./components/App").default;
     render(NextApp);
   });
