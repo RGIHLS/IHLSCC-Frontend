d8eea165bcdee76df8c32ab77e68bc1ab7c09bb7
Dylan McDonald <dylan.mcdonald@inhouselegalsolutions.com>
2024-07-25 11:58:18 +0100

    chore: Update taskpane index to use TypeScript syntax for hot module replacement

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -34,8 +34,8 @@ Office.onReady(() => {
   render(App);
 });
 
-if (module.hot) {
-  module.hot.accept("./components/App", () => {
+if ((module as any).hot) {
+  (module as any).hot.accept("./components/App", () => {
     const NextApp = require("./components/App").default;
     render(NextApp);
   });
